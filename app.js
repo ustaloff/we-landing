e7402286e8913cf47e00742e32795e066f706383
Kirills Ustalovs <kirils.u@rdvit.com>
2025-11-24 19:38:34 +0200

    Added swiper

--- app.js
+++ app.js
@@ -2,13 +2,38 @@ document.getElementById('myButton').addEventListener('click', function() {
     alert('Button clicked!');
 });
 
-const swiper = new Swiper('.swiper', {
+const offersSwiper = new Swiper('.offers__swiper', {
     // Optional parameters
     loop: true,
     slidesPerView: 4,
     slidesPerGroup: 1,
     slidesPerColumn: 1,
     spaceBetween: 32,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+});
+
+const gamesSwiper = new Swiper('.games__swiper', {
+    // Optional parameters
+    loop: true,
+    slidesPerView: 6,
+    slidesPerGroup: 1,
+    slidesPerColumn: 2,
+    spaceBetween: 7,
 
     // If we need pagination
     pagination: {
